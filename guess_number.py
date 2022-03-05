00fbd69ceebb69423dabf31a01cb6c8f127f082b
Chiang <tien23@gmail.com>
2022-03-06 07:54:09 +0800

    add count

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,15 @@
 import random
 number = random.randint(1,10)
 
+count = 0
 
 while True :
     answer = input('請猜數字1 - 10: ')
     answer = int(answer)
+    count += 1
     if answer == number :
         print('猜對了', number)
+        print('猜了',count,'次')
         break
     elif answer > number :
         print('小於', answer)
